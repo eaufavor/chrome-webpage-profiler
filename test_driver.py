8769cbc80ee136823d168f4296d5905dff193c60
Yuchen Wu <ywu@akamai.com>
2015-07-29 16:36:20 -0400

    Typo

--- test_driver.py
+++ test_driver.py
@@ -69,7 +69,7 @@ def loader_worker(my_id, default, job_queue, result_queue):
             result = loader.load_page(testJob[0], testJob[1])
             if result:
                 result_queue.put(result)
-            if result.status() == LoadResult.FAILURE_TIMEOUT:
+            if result.status == LoadResult.FAILURE_TIMEOUT:
                 loader.teardown()
                 if not loader.setup(my_id):
                     logging.error('Error setting up loader')
